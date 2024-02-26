cebc0b680008d8cc23306040d73e92c92404e3f9
Rogergrosado <rgrosado1@gmail.com>
2024-02-26 15:13:43 -0400

    fix

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * _puts - prints a string,
@@ -10,9 +9,9 @@
  */
 void _puts(char *str)
 {
-for (;*str != '\0'; str++)
-{
+while (*str)
+
 _putchar(*str++);
-}
 _putchar('\n');
+return;
 }
