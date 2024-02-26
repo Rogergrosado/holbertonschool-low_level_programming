61ba44f8a99c0d619151181258ccf1bfa18d112b
Rogergrosado <rgrosado1@gmail.com>
2024-02-26 16:11:07 -0400

    fix

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- * _puts - prints a string,
- * followed by a new line, to stdout
- * @str: string to print
- *
- * Return: void
- */
+*_puts - prints a string,
+*followed by a new line, to stdout
+*@str: string to print
+*
+*Return: void
+*/
 void _puts(char *str)
 {
-while (*str)
+    while(*str)
 
-_putchar(*str++);
-_putchar('\n');
-return;
+        _putchar(*str++);
+    _putchar('\n');
+    return;
 }
