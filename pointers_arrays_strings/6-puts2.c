cc44ee2431b67034da55c6b8d4a74e4859958050
Rogergrosado <rgrosado1@gmail.com>
2024-02-26 23:21:54 -0400

    fix

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,15 +1,19 @@
 #include "main.h"
-
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * puts2 - prints every other character of a string
+ * @str: string to print
+ * Return: void
  */
-int main(void)
-{
-	char *str;
 
-	str = "0123456789";
-	puts2(str);
-	return (0);
+void puts2(char *str)
+{
+int i = 0;
+
+while (str[i] != '\0')
+{
+if (i % 2 == 0)
+_putchar(str[i]);
+i++;
+}
+_putchar('\n');
 }
