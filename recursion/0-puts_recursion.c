02490e15705dc110274f68c39a1c3b72364dce99
Rogergrosado <rgrosado1@gmail.com>
2024-03-04 12:19:10 -0400

    making C - Recursion

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts_recursion - reverses a string
+ * @s: pointer to  data of string
+ * Return: it returns nothing
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
+
