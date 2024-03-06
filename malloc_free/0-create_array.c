ed216fde86ce3c5096416f674b5f88106a78b46c
Rogergrosado <rgrosado1@gmail.com>
2024-03-06 12:00:38 -0400

    making C - malloc, free

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * create_array - This function creates an array of size.
+ * @size: size of the array
+ * @c: inputted string
+ * Return: it returns 0 if the program compiles ok
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *ret;
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+	ret = malloc(size);
+
+	if (ret == NULL)
+		return (ret);
+
+	for (i = 0 ; i < size ; i++)
+	{
+		ret[i] = c;
+	}
+	return (ret);
+}
